a89cc28a506174fec8da198956601ecd4accced1
Randy Steele <steelerx155@Gmail.com>
2023-04-24 18:39:22 -0400

    added app.js and started styling header

--- .history/app_20230424175609.js
+++ .history/app_20230424175609.js
@@ -4,5 +4,4 @@ const header = document.querySelector(".header.container");
 
 hamburger.addEventListener('click',() => {
     hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
 });
